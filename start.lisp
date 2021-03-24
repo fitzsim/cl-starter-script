1426f6bc7e641f1243058d15a9b9814ce98a0a72
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2021-03-23 21:09:43 -0400

    start.lisp: New file

---
+++ start.lisp
@@ -0,0 +1,64 @@
+#| start.lisp --- a single-file self-contained Common Lisp script template
+export __CL_ARGV0="$0"
+exec sbcl --script "$0" "$@"
+
+Copyright (C) 2021 Thomas Fitzsimmons
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+|#
+
+(cl:require "asdf")
+(asdf:initialize-source-registry
+ '(:source-registry :ignore-inherited-configuration (:tree :here)))
+(cl:let ((cl:*error-output* (cl:make-string-output-stream))) ; version parsing
+  (asdf:load-system :net.didierverna.clon))
+
+(cl:defpackage #:start)
+(cl:in-package #:start)
+
+(net.didierverna.clon:nickname-package)
+
+(clon:defsynopsis (:postfix "FILES...")
+  (text :contents "Starter utility script.")
+  (group (:header "Flags:")
+	 (flag :short-name "h" :long-name "help"
+	       :description "Print this help and exit.")
+	 (flag :short-name "q" :long-name "quiet"
+	       :description "Print fewer messages."))
+  (group (:header "Options:")
+	 (path :short-name "f" :long-name "file"
+	       :description "Output to FILE."
+	       :argument-name "FILE"
+	       :type :file
+	       :default-value #p"output.txt")))
+
+(cl:defun main ()
+  "Entry point for the script."
+  (clon:make-context)
+  (cl:cond ((clon:getopt :short-name "h")
+	    (clon:help))
+	   ((cl:not (clon:getopt :short-name "q"))
+	    (cl:format cl:t "Script full path:  ~A~%" cl:*load-pathname*)
+	    (cl:format cl:t "Script argument 0: ~A~%" (uiop:argv0))
+	    (cl:format cl:t "ASDF version:      ~A~%" (asdf:asdf-version))
+	    (cl:format cl:t "Program name:      ~A~%" (clon:progname))
+	    (cl:format cl:t "Options:")
+	    (clon:do-cmdline-options (option name value source)
+	      (cl:print (cl:list option name value source)))
+	    (cl:terpri)
+	    (cl:format cl:t "Remainder:         ~A~%" (clon:remainder))))
+  (cl:when (uiop:argv0)
+    (uiop:quit)))
+
+(cl:when (uiop:argv0)
+  (main))
